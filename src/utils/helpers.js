60c2a7d7d5ec2f1465d1356ee1e2553e21cb13d1
Nick Perdomo <nickperdomo121@gmail.com>
2019-06-19 13:11:52 -0700

    task: Create helper to format Question object

---
+++ src/utils/helpers.js
@@ -0,0 +1,24 @@
+export function formatQuestion(autherUser, user, question) {
+  const {
+    name,
+    avatarURL,
+  } = user
+
+  const { 
+    id,
+    author,
+    timestamp,
+    optionOne,
+    optionTwo,
+  } = question
+
+  return {
+    name,
+    id,
+    author,
+    avatar: avatarURL,
+    timestamp,
+    optionOne,
+    optionTwo,
+  }
+}
