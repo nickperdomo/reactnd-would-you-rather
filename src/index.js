e1a958d3935db7bc64a7070c10d64c53f2bae51b
Nick Perdomo <nickperdomo121@gmail.com>
2019-06-21 10:46:37 -0700

    task: Add react-router to app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from "react-router-dom"
 import reducer from './reducers'
 import middleware from './middleware'
-import './index.css';
-import App from './views/App';
+import './index.css'
+import App from './views/App'
 
 const store = createStore(reducer, middleware)
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>, 
   document.getElementById('root'));
