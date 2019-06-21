8f5b3a851209684eeae6125de88b2631c43082c4
Nick Perdomo <nickperdomo121@gmail.com>
2019-06-21 10:41:24 -0700

    feat: Add user avatar to Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { NavLink, Link } from 'react-router-dom'
 import './Nav.scss'
 
 class Nav extends Component {
   render() {
+    const {
+      authedUser,
+      users
+    } = this.props
+
     return (
       <nav className='nav'>
         <div className='navInner'>
           <ul>
-            <li key='home'>Home</li>
-            <li key='newQuestion'>New Question</li>
-            <li key='leaderboard'>Leaderboard</li>
+            <li key='home'>
+              <NavLink 
+                exact to="/questions"
+                activeClassName="navActiveView"
+                >
+                  Home
+              </NavLink>
+            </li>
+            <li key='newQuestion'>
+              <NavLink 
+                exact to="/add"
+                activeClassName="navActiveView"
+                >
+                  New Question
+              </NavLink>
+            </li>
+            <li key='leaderboard'>
+              <NavLink 
+                exact to="/leaderboard"
+                activeClassName="navActiveView"
+                >
+                  Leaderboard
+              </NavLink>
+            </li>
           </ul>
           <div className='signOutContainer'>
-            <span>Sign out</span>
-            <img src='' alt='' />
+              <Link 
+                exact to="/"
+                >
+                  <span>Sign out</span>
+                  <img src={users[authedUser].avatarURL} alt={`${users[authedUser].name}'s avatar`} />
+              </Link>
           </div>
         </div>
       </nav>
@@ -21,4 +53,11 @@ class Nav extends Component {
   }
 }
 
-export default Nav
+function mapStateToProps({authedUser, users}) {
+  return {
+    authedUser,
+    users
+  }
+}
+
+export default connect(mapStateToProps)(Nav)
