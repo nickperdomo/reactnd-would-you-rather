86f3d22ffaf922fcbf10d510a478b35ae2989bd2
Nick Perdomo <nickperdomo121@gmail.com>
2019-07-07 13:30:46 -0700

    fix: Nav is embedded in multiple views instead of root view

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import './Home.scss'
-import Nav from '../components/Nav'
 import QuestionToggle from '../components/QuestionToggle'
 import QuestionCard from '../components/QuestionCard'
 
