9ccc89e0a5bc36cdd9f8b10a2703b76e2340f2e7
Nick Perdomo <nickperdomo121@gmail.com>
2019-07-08 10:18:36 -0700

    task: Remove remnant cardMode definition object

--- src/views/shared.js
+++ src/views/shared.js
@@ -1,10 +1,3 @@
-export const cardMode = {
-  question: 'question',
-  newQuestion: 'newQuestion',
-  poll: 'poll',
-  pollResults: 'pollResults',
-}
-
 export function hasAnswered(authedUser, question) {
   const votes = [
     ...question.optionOne.votes,
