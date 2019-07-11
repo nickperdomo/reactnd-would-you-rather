bed20703e06b356f55721f7b72ec54ba384bc48a
Nick Perdomo <nickperdomo121@gmail.com>
2019-07-11 14:22:28 -0700

    feat: Add loading bar back into app

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,22 +1,16 @@
 import { getInitialData } from '../utils/api'
 import { receiveUsers } from './users'
 import { receiveQuestions } from './questions'
-// import { setAuthedUser } from './authedUser'
-// import { showLoading, hideLoading } from 'react-redux-loading'
-
-// const AUTHED_ID = 'sarahedo'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 export function handleInitalData() {
   return (dispatch) => {
-    // dispatch(showLoading())
+    dispatch(showLoading())
     return getInitialData()
       .then(({ users, questions }) => {
         dispatch(receiveUsers(users))
         dispatch(receiveQuestions(questions))
-        // dispatch(setAuthedUser(
-        //   Object.keys(users).sort((a,b) => (a.name > b.name) ? 1 : -1)[0])
-        // )
-        // dispatch(hideLoading())
+        dispatch(hideLoading())
       })
   }
 }
--- src/views/App.js
+++ src/views/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
-// import LoadingBar from 'react-redux-loading'
+import LoadingBar from 'react-redux-loading'
 import { handleInitalData } from '../actions/shared'
 import PrivateRoute from '../components/PrivateRoute'
 import Nav from '../components/Nav'
@@ -15,34 +15,46 @@ import './App.scss';
 
 
 class App extends Component {
+  state = {
+    loading: true
+  }
+
   componentDidMount() {
     this.props.dispatch(handleInitalData())
+    this.setState(() => ({
+      loading: false
+    }))
   }
 
   render() {
-    // const authedUser = this.props.authedUser
+    const loading = this.state.loading
 
     return (
-      <div className="App">  
-        <Nav />
-        <Switch>
-          <Route exact path='/signin' component={SignIn} />
-          <PrivateRoute exact path='/' component={Home} />
-          <PrivateRoute exact path='/add' component={NewQuestion} />
-          <PrivateRoute exact path='/questions/:question_id' component={Poll} />
-          <PrivateRoute exact path='/leaderboard' component={Leaderboard} />
-          <Route component={NotFound} />
-        </Switch>
+      <div className="App">
+        <LoadingBar
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            backgroundColor: '#0d68cf'
+        }} /> 
+        {loading
+          ? null
+          : <>
+              <Nav />
+              <Switch>
+                <Route exact path='/signin' component={SignIn} />
+                <PrivateRoute exact path='/' component={Home} />
+                <PrivateRoute exact path='/add' component={NewQuestion} />
+                <PrivateRoute exact path='/questions/:question_id' component={Poll} />
+                <PrivateRoute exact path='/leaderboard' component={Leaderboard} />
+                <Route component={NotFound} />
+              </Switch>
+            </>
+        }
       </div>
     )
   }
 }
 
-function mapStateToProps({authedUser}) {
-  return {
-    loading: authedUser === null,
-    authedUser,
-  }
-}
-
-export default connect(mapStateToProps)(App);
+export default connect()(App);
