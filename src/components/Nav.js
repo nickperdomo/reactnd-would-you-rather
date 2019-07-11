9b9d2c9c5b634c7c8c8d0ead6c5a7d3176ccd698
Nick Perdomo <nickperdomo121@gmail.com>
2019-07-10 22:06:29 -0700

    task: Prevent nav from rendering on /signin

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,58 +1,85 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Route, withRouter } from 'react-router-dom'
 import { NavLink, Link } from 'react-router-dom'
 import './Nav.scss'
+import { setAuthedUser } from '../actions/authedUser'
 
 class Nav extends Component {
+  
+  handleSignout = (e) => {
+    e.preventDefault()
+    
+    const { 
+      dispatch,
+      history
+    } =this.props
+
+    dispatch(setAuthedUser(null))
+    history.replace('/signin')
+  }
+
   render() {
     const {
       authedUser,
-      users
+      users,
+      location,
     } = this.props
 
     return (
-      <nav className='nav'>
-        <div className='navInner'>
-          <ul>
-            <li key='home'>
-              <NavLink 
-                exact to="/questions"
-                className="navLink"
-                activeClassName="navActiveView"
-                >
-                  Home
-              </NavLink>
-            </li>
-            <li key='newQuestion'>
-              <NavLink 
-                exact to="/add"
-                className="navLink"
-                activeClassName="navActiveView"
-                >
-                  New Question
-              </NavLink>
-            </li>
-            <li key='leaderboard'>
-              <NavLink 
-                exact to="/leaderboard"
-                className="navLink"
-                activeClassName="navActiveView"
-                >
-                  Leaderboard
-              </NavLink>
-            </li>
-          </ul>
-          <div className='signOutContainer'>
-              <Link 
-                to="/signin"
-                className="navLink"
-                >
-                  <span>Sign out</span>
-                  <img src={users[authedUser].avatarURL} alt={`${users[authedUser].name}'s avatar`} />
-              </Link>
-          </div>
+      <>
+      {location.pathname.match('/signin')
+        ? null
+        : <nav className='nav'>
+          <div className='navInner'>
+            <ul>
+              <li key='home'>
+                <NavLink 
+                  exact to="/questions"
+                  className="navLink"
+                  activeClassName="navActiveView"
+                  >
+                    Home
+                </NavLink>
+              </li>
+              <li key='newQuestion'>
+                <NavLink 
+                  exact to="/add"
+                  className="navLink"
+                  activeClassName="navActiveView"
+                  >
+                    New Question
+                </NavLink>
+              </li>
+              <li key='leaderboard'>
+                <NavLink 
+                  exact to="/leaderboard"
+                  className="navLink"
+                  activeClassName="navActiveView"
+                  >
+                    Leaderboard
+                </NavLink>
+              </li>
+            </ul>
+            <div className='signOutContainer'>
+                {/* <Link 
+                  to="/signin"
+                  className="navLink"
+                  >
+                    <span>Sign out</span>
+                    <img src={users[authedUser].avatarURL} alt={`${users[authedUser].name}'s avatar`} />
+                </Link>
+                <Route path="/signin" component={SignIn} /> */}
+              <button
+                onClick={this.handleSignout}
+              >
+                Sign Out
+              </button>   
+            </div>
         </div>
       </nav>
+      }
+      </>
     )
   }
 }
@@ -64,4 +91,4 @@ function mapStateToProps({authedUser, users}) {
   }
 }
 
-export default connect(mapStateToProps)(Nav)
+export default withRouter(connect(mapStateToProps)(Nav))
